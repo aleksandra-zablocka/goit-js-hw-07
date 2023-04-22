b9a6eda4c40cd1aff483f338358ae5cc78e8b95b
Aleksandra Zabłocka <aleksandra-zablocka@o2.pl>
2023-04-22 19:03:18 +0200

    Added loop for

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const gallery = document.querySelector(".gallery");
+
+createGalleryItems();
+function createGalleryItems() {
+  const items = [];
+  for (let i = 0; i < galleryItems.length; i++) {
+    const item = document.createElement("button");
+    item.type = "button";
+    item.style.backgroundImage = "";
+    item.classList.add("item");
+    items.push(item);
+  }
+  gallery.append(...items);
+}
+
+// const imageMarkup = createMarkup(galleryItems);
+// function createMarkup() {
+//   return galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `
+//         <li class="gallery__item">
+//         <a class="gallery__link" href=${original}">
+//         <img class="gallery__image" src="${preview} data-source=${original} alt="${description}"/></a>
+//         </li>
+//         `;
+//     })
+//     .join("");
+// }
+// console.log(imageMarkup);
 console.log(galleryItems);
